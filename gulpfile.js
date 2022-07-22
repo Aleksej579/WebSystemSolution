c0172acad08443f612ed60003b8e3c2a30905921
root <admin@example.com>
2022-07-22 15:40:52 +0300

    feat:(start template, gulp)

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+const gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    htmlmin = require('gulp-htmlmin'),
+    autoprefixer = require('gulp-autoprefixer');
+
+function minifyHtml() {
+    return (
+        gulp
+            .src("src/*.html")
+            .pipe(htmlmin({ collapseWhitespace: true }))
+            .pipe(gulp.dest('./dist'))
+    );
+}
+
+function style() {
+    return (
+        gulp
+            .src("src/style/*.css")
+            .pipe(autoprefixer({
+                cascade: false
+            }))
+            .pipe(concat('styles.css'))
+            .pipe(gulp.dest("./dist"))
+    );
+}
+
+function fonts() {
+    return (
+        gulp
+            .src('src/fonts/**/*.{eot,svg,ttf,woff,woff2}')
+            .pipe(gulp.dest('./dist/fonts/'))
+    )
+}
+function images() {
+    return (
+        gulp
+            .src('src/images/**/*.{png, jpeg, svg}')
+            .pipe(gulp.dest('./dist/images/'))
+    )
+}
+function js() {
+    return (
+        gulp
+            .src('src/javascript/*.js')
+            .pipe(concat('main.js'))
+            .pipe(gulp.dest('./dist'))
+    );
+}
+function watch() {
+    gulp.watch('src/*.html', minifyHtml)
+    gulp.watch('src/style/*.css', style)
+    gulp.watch('src/javascript/*.js', js)
+}
+
+module.exports.start = gulp.series(minifyHtml, style, js, fonts, images, watch)
