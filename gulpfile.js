797abb5c8de7f864f7b670f6d925c74a23686061
root <admin@example.com>
2022-07-23 00:22:10 +0300

    feat: (smacss, font, img, ...)

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp'),
     concat = require('gulp-concat'),
     htmlmin = require('gulp-htmlmin'),
     autoprefixer = require('gulp-autoprefixer');
-
+    
 function minifyHtml() {
     return (
         gulp
@@ -27,15 +27,15 @@ function style() {
 function fonts() {
     return (
         gulp
-            .src('src/fonts/**/*.{eot,svg,ttf,woff,woff2}')
-            .pipe(gulp.dest('./dist/fonts/'))
+            .src('src/fonts/*.{eot,svg,ttf,woff,woff2}')
+            .pipe(gulp.dest('./dist/fonts'))
     )
 }
 function images() {
     return (
         gulp
-            .src('src/images/**/*.{png, jpeg, svg}')
-            .pipe(gulp.dest('./dist/images/'))
+            .src('src/assets/*')
+            .pipe(gulp.dest('./dist/assets'))
     )
 }
 function js() {
@@ -50,6 +50,8 @@ function watch() {
     gulp.watch('src/*.html', minifyHtml)
     gulp.watch('src/style/*.css', style)
     gulp.watch('src/javascript/*.js', js)
+    gulp.watch('src/assets/*', images)
+    gulp.watch('src/fonts/*', fonts)
 }
 
 module.exports.start = gulp.series(minifyHtml, style, js, fonts, images, watch)
