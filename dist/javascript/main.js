1b1f266969a11927864abb55f9584b2a6584a9a7
root <admin@example.com>
2022-07-24 14:48:19 +0300

    fix:(add class when click)

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -13,3 +13,27 @@ mobMenu.onclick = function(){
 	}
 };
 
+// Read more
+let blockShortText = document.querySelector('#service-description__wrapper');
+let btnReadMore = document.querySelector('#read-more');
+
+btnReadMore.onclick = function(){
+	blockShortText.style.maxHeight = "100%";
+	btnReadMore.style.display = 'none';
+};
+
+//Hightlight item select menu
+let arrLi = nav.querySelectorAll('ul li')
+nav.addEventListener('click', e => {
+	arrLi.forEach(item => item.classList.remove('is-select_nav'))
+	e.target.parentElement.classList.add('is-select_nav');
+})
+
+//Hightlight item select language button
+let blockLangSwitch = document.querySelector('.header__language-switch');
+
+let arrBtn = blockLangSwitch.querySelectorAll('li')
+blockLangSwitch.addEventListener('click', e => {
+	arrBtn.forEach(item => item.classList.remove('is-select_lang'))
+	e.target.parentElement.classList.add('is-select_lang');
+})
